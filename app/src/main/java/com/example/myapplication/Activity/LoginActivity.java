9e1963de2d92ddc414e974bc90b04524af4caaf6
milygc0611 <milygcenteno@gmail.com>
2024-06-29 16:11:24 -0400

    segunda commit

---
+++ app/src/main/java/com/example/myapplication/Activity/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.example.myapplication.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplication.MainActivity;
+import com.example.myapplication.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        Button Txtaceptar = findViewById(R.id.txtaceptar);
+
+        Txtaceptar.setOnClickListener(view -> {
+
+            Intent VentanaPrincipal = new Intent(this, MainActivity.class);
+
+            startActivity(VentanaPrincipal);
+
+        });
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
